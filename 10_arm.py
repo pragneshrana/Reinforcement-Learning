5fd8649caa89f3f44a8cfa2aa3883f0a98779e59
pragneshrana <pragneshrana244@gmail.com>
2020-02-02 21:52:36 +0530

    almost done

--- 10_arm.py
+++ 10_arm.py
@@ -7,6 +7,7 @@ class epsilon_greedy_algo():
     '''
     Initializing the variables
     '''
+    #for one bandit problem or 2000 diff bandit problem
     self.no_of_arms = no_of_arms  #number of arams
     self.mu = mu #given mean for distribution of arms
     self.sigma = sigma #given sigma for distribution of arms
@@ -15,30 +16,29 @@ class epsilon_greedy_algo():
     self.no_of_plays = no_of_plays #given plays in each round #number of steps
     self.avg_reward = np.zeros(no_of_plays)
     self.no_of_bandit_prob = no_of_bandit_prob #different type of bandit problems
+
+    #for specific bandit problem or 1000 play
     self.reward = np.zeros(no_of_arms) #avg reward for all arm 
     self.selected_arm_index = None #arm index of selected arm
     self.selected_arm_mean = None #assign mean of selected arm  
     self.selected_arm_sigma = 1
     self.reward_after_each_play = None #reward after each play
+    self.counter_of_selected_arm = np.zeros(no_of_arms)
 
   def distribution_to_each_arm(self):
     '''
     Assinging the random mean to each arms
     '''
     self.arms = np.random.normal(self.mu, self.sigma, self.no_of_arms) #mean assigned to every arm
+    # print('self.arms: ', self.arms)
+    # print('print: ', print)
+    
 
   def intial_reward(self):
     '''
     Intialize every arm with zero reward
     '''
-    self.reward = np.zeros(no_of_arms) 
-  
-  def each_play(self):
-    '''
-    Each play of bandits
-    In which it picks the arm with best reward based on previous reward records
-    '''
-    #pick an arm 
+    self.reward = np.zeros(self.no_of_arms) 
   
   def random_pick(self):
     '''
@@ -48,28 +48,41 @@ class epsilon_greedy_algo():
     or
     epsilon times it will arm randomly
     '''
-    self.selected_arm_index = random.choice(range(len(self.no_of_arms)))
-    self.selected_arm_mean = self.arms[arm_index]
+    self.selected_arm_index = random.choice(range(self.no_of_arms))
+    self.selected_arm_mean = self.arms[self.selected_arm_index]
   
-  def greedy_arm_pick(self):
-    '''
-    It will pick the arm which has highest reward in previous rum
-    '''
-    self.selected_arm_index = 
-    self.selected_arm_mean = 
-  
-
   def obtain_reward(self):
     '''
     once arm is picked obtain reward from specified distribution of picked arm
     '''
     self.reward_after_each_play = np.random.normal(self.selected_arm_mean, self.selected_arm_sigma)
   
+  def update_counter_of_selected_arm(self):
+    '''
+    update counter for selected arms
+    '''
+    self.counter_of_selected_arm[self.selected_arm_index] += 1 
+    
+  
   def update_reward(self):
     '''
     This methos will update reward after each play
     '''
-    self.reward[self.selected_arm_index] += self.reward_after_each_play
+    count = self.counter_of_selected_arm[self.selected_arm_index] #just to reduce size of eqaution
+    self.reward[self.selected_arm_index] = (self.reward[self.selected_arm_index]*(count-1)+ self.reward_after_each_play)/count
+
+  def epsilon_greedy_arm_pick(self):
+    '''
+    It will pick the arm which has highest reward in previous rum
+    '''
+    if(random.random() > (1-self.epsilon)):
+      self.random_pick()
+    else:
+      self.selected_arm_index = np.argmax(self.reward)
+      # print('self.reward: ', self.reward)
+      # print(self.selected_arm_index)
+      self.selected_arm_mean = np.max(self.reward_after_each_play)
+      # print(self.selected_arm_mean)
   
   def avg_reward_update(self,play_index):
     '''
@@ -77,14 +90,52 @@ class epsilon_greedy_algo():
     '''
     self.avg_reward[play_index] +=  self.reward_after_each_play
   
+  def chekc_result(self):
+    '''
+    chekc code by printing output
+    '''
+    print('Arm picked:',self.selected_arm_index)
+    print('Reward_after_each_play :',self.reward_after_each_play)
+    print('Rewads with each arm',self.reward)
+    print('Avg reward :',self.avg_reward)
+    print('counter_of_selected_arm: ', self.counter_of_selected_arm)
+    print('self.selected_arm_mean: ', self.selected_arm_mean)
+
+    print('\n')
+
   def simulate_epsilon_greedy_policy(self):
     '''
     This method simulates.
     It will pick the arm which has highest overall reward in previous plays.
     '''
-    self.distribution_to_each_arm()
-
+    for i in range(self.no_of_bandit_prob):
+      print('Bandit problem: ', i)
+      self.distribution_to_each_arm()
+      self.intial_reward()
+      #zeroth play
+      self.random_pick()
+      self.obtain_reward()
+      self.update_counter_of_selected_arm()
+      self.update_reward()
+      self.avg_reward_update(0)
+      # self.chekc_result()
+      for j in range(1,self.no_of_plays):
+        self.epsilon_greedy_arm_pick()
+        self.obtain_reward()
+        self.update_counter_of_selected_arm()
+        self.update_reward()
+        self.avg_reward_update(j)
+        # self.chekc_result()
+    final_avg_reward = self.avg_reward/self.no_of_bandit_prob
+    return final_avg_reward
 
 print('hello')
-greedy = epsilon_greedy_algo(no_of_arms=5,mu=0,sigma=1,epsilon=0,no_of_plays=10,no_of_bandit_prob=10)
-greedy.simulate()
+no_of_plays = 1000
+greedy = epsilon_greedy_algo(no_of_arms=3,mu=0,sigma=1,epsilon=0,no_of_plays=no_of_plays,no_of_bandit_prob=2000)
+avg_reward = greedy.simulate_epsilon_greedy_policy()
+print(len(avg_reward))
+import matplotlib.pyplot as plt
+plt.plot(avg_reward)
+plt.xlabel('No of plays of each bandit problem')
+plt.ylabel('Avg Reward')
+plt.show()
