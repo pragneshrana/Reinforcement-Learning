3ef4c7215c0a531ff97700e12e919616a3be86ef
pragneshrana <pragneshrana244@gmail.com>
2020-05-25 23:38:11 +0530

    code of Q1

---
+++ Assignment_3/Try1/Q1_SMDP/Q1.py
@@ -0,0 +1,10 @@
+# The code red reference is given below:
+
+# Some GridWorld environments for OpenAI Gym by Castellini Jacopo.
+# Source : https://github.com/opocaj92/GridWorldEnvs
+
+# Four room gird world environment is taken from Aharutyu git
+# Source : https://github.com/aharutyu/gym-gridworlds/blob/master/gridworlds/envs/four_rooms.py
+
+import numpy as np
+
